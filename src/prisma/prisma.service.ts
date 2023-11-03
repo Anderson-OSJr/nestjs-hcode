27ca85ba50571612cf2d69bf9614a42196a1d1ae
Anderson OSJr <anderosj@gmail.com>
2023-11-03 09:50:45 -0300

    version after have Prisma && MySQL db configuration

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,26 @@
+/* eslint-disable prettier/prettier */
+import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit{
+    
+    // esse código abaixo fica disponível na documentação do Nest e do Prisma
+
+    async onModuleInit() {
+        this.$connect;
+    }
+
+    async enableShutdownHooks(app: INestApplication) {
+        process.on('beforeExit', async () => {
+            await app.close();
+        })
+    }
+
+    /* Essa é a solução recomendada pela documentação tanto do prisma quanto do NestJS.
+
+https://www.prisma.io/docs/guides/upgrade-guides/upgrading-versions/upgrading-to-prisma-5#removal-of-the-beforeexit-hook-from-the-library-engine
+
+https://docs.nestjs.com/fundamentals/lifecycle-events#application-shutdown */
+
+}
